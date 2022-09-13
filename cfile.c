b7d7d1c6e66db0689b647cef2a8cbc025cf7d1b1
AnishKumar-AK <anishkumar4724@gmail.com>
2022-09-13 12:03:23 +0530

    4
    1

---
+++ cfile.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int num1 = 10;
+  int num2 = 20;
+  int sum;
+  sum = num1 + num2;
+       printf("sum of num1 and num2 is : %d \n",sum);
+       getch();
+}
